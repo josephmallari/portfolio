4eb542d71fccac259d105b1e8da5ae6b5455563a
Joseph Mallari <joseph.mallari@codeandtheory.com>
2018-05-04 14:21:36 -0400

    updatse

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-console.log('hi!');
-console.log(window.screen.width);
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.intro').classList.add('reveal');
+
+    setTimeout(function() {
+        document.querySelector('.current').classList.add('reveal');
+    }, 200);
+
+    setTimeout(function() {
+        document.querySelector('.projects').classList.add('reveal');
+    }, 400);
+
+    setTimeout(function() {
+        document.querySelector('.social').classList.add('reveal');
+    }, 600);
+});
